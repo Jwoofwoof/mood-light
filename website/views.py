f9ee726fd63e54ba6907cc17d8802a0df5a34a85
jasung <jason2137475@gmail.com>
2022-11-29 15:28:48 -0800

    added more color and session

--- website/views.py
+++ website/views.py
@@ -1,14 +1,18 @@
-from flask import Blueprint, render_template, request, session
+from flask import Blueprint, render_template, request
+
 
 views = Blueprint('views', __name__)
+session ={}
 
 
-@views.route('/', methods=['POST'])
+@views.route('/', methods=['GET','POST'])
 def home():
+
+    print(session)
     if request.method == 'POST':
         #data = request.get_json(silent=True)
         data = request.form
-        session["mood"] = data["mood"]
+        session["mood"] = str(data["mood"])
         session["tempo"] = data["tempo"]
     print(session)
     return session["mood"]
@@ -17,14 +21,33 @@ def home():
 
 @views.route('/map', methods=['GET', 'POST'])
 def map():
+
+    print(session)
     if request.method == 'POST':
         data = request.form
         session["mapping"] = data
+        print(session)
+
+
     return render_template("sign_up.html")
 
 
-@views.route('/getInfo', methods=['GET'])
+@views.route('/getInfo', methods=['GET', 'POST'])
 def getInfo():
-    print(session["mapping"])
+    #print(session["mood"])
     # return tempo (session["tempo"]) and color (session[session["mood"]])
-    return [session["mapping"], session["mapping"][session["mood"]]]
+    #mood = session["mood"]
+    if "mapping" in session: 
+        print(session["mapping"])
+        return {"tempo": session["tempo"],  "color": session["mapping"][session["mood"]]}
+    else:
+        return "1"
+ 
+
+
+@views.route('/test', methods=['GET', 'POST'])
+def test():
+    #print(session["mood"])
+    # return tempo (session["tempo"]) and color (session[session["mood"]])
+    #mood = session["mood"]
+    return "2"
